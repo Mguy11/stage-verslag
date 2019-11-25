cf4605abb02126e10b1ea4efead9973d551ce571
Martijn Bankert <martijnb@LAN.LOCAL>
2019-11-25 17:11:36 +0100

    removed backgrounds contentblocks

--- src/components/smallheader.js
+++ src/components/smallheader.js
@@ -7,7 +7,7 @@ export default class SmallHeader extends Component {
 
     render () {
         return (
-          <div className="header">
+          <div className="header header--small">
                 <span className="header__title header__title--small">{this.props.title}</span>
                 <figure className="header__image-wrapper header__image-wrapper--small">
                     <img className="header__image"/>
