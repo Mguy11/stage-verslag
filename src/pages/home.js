0b83e4f984585497d9524d4a10c96549450677bf
Martijn Bankert <martijnb@LAN.LOCAL>
2019-11-29 16:45:25 +0100

    rewrote content block

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,14 +18,18 @@ const Home = () => {
             <div className="wrap">
                 <Grid style={{marginTop: '2rem'}}>
                     <Row>
-                            <ContentBlock />      
+                            <ContentBlock 
+                                title = ""
+                                body = "Deze website heb ik gebouwd voor de verslaglegging van mijn 3e jaars stage. 
+                                Ik heb mijn stage gelopen bij DotControl in Rotterdam. DotControl is samen met RockBoost een full service web bureau. "
+                            />      
                     </Row>
                     <Row style={{margin: '2rem 0'}}>
                         <Col xs={6}>
-                            <div className="content__block content__block--grey">
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                </p>
-                            </div>
+                            <ContentBlock 
+                                title = ""
+                                body = ""
+                            />
                         </Col>
                         <Col xs={6}>
                             <div className="content__image">
